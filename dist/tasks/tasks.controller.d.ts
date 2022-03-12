a87cdcc612c68079d60537f81587bf2d3fcb46d0
JanDR <jan.duinkerken@udc.es>
2022-03-13 00:18:52 +0100

    Get tasks and create tasks

---
+++ dist/tasks/tasks.controller.d.ts
@@ -0,0 +1,8 @@
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+export declare class TasksController {
+    private tasksService;
+    constructor(tasksService: TasksService);
+    getAllTasks(): Task[];
+    createTask(title: any, description: any): Task;
+}
